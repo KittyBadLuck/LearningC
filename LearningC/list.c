97b75ee4c7825f07294a3fdc8e5e26133939fbf0
KittyBadLuck <117767124+KittyBadLuck@users.noreply.github.com>
2024-07-02 15:21:58 -0400

    delete in list

--- LearningC/list.c
+++ LearningC/list.c
@@ -46,6 +46,45 @@ list* array_to_list(int d[], int size)
 	return head;
 }
 
+void delete(list* elem, list** prev, list** hd)
+{
+	if (*hd == *prev)
+		*hd = elem->next;
+	else
+		(*prev)->next = elem->next;
+	free(elem);
+}
+
+void delete_list(list* h)
+{
+	list* temp;
+	if (h != NULL)
+	{
+		temp = h;
+		delete_list(h = h->next);
+		free(temp);
+	}
+}
+
+void concat(list* h1, list* h2)
+{
+	if (h1 == NULL)
+		h1 = h2;
+	else if (h1->next == NULL)
+		h1->next = h2;
+	else
+		concat(h1->next, h2);
+}
+int count(list* h)
+{
+	int cnt = 0;
+	while (h != NULL)
+	{
+		cnt++;
+		h = h->next;
+	}
+	return cnt;
+}
 int main(void)
 {
 	list list_of_int;
@@ -56,13 +95,30 @@ int main(void)
 	head->data = 5; 
 	head->next = NULL;
 
-	print_list(head, "single element list");
-	printf("\n\n");
+	//print_list(head, "single element list");
+	//printf("\n\n");
 
 	int data[6] = { 2,3,5,7,8,9 };
 	head = array_to_list(data, 6);
 
-	print_list(head, "array to list ");
+	//print_list(head, "array to list ");
+	//printf("\n\n");
+
+	//test delete 
+	list* head1 = NULL, * head2 = NULL;
+	int data2[] = { 17,6,99 };
+	head1 = array_to_list(data, 6);
+	head2 = array_to_list(data2, 3);
+	print_list(head1, "data[6] made into a 6 elem list");
+	print_list(head2, "data2[3] made into a 3 elem list");
+	concat(head1, head2);
+	print_list(head1, "concat list");
+	printf("\n Size of concat list is %d\n", count(head1));
+	delete(head1, &head1, &head1);
+	print_list(head1, "delete from list");
+	delete(head1->next->next, &head1->next, &head1);
+	print_list(head1, "delete from list");
+
 	printf("\n\n");
 
 	return 0;
